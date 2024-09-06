7b34131553d70e35235a999794d69e375871ebe5
betashort <wshota.betashort@gmail.com>
2024-09-06 23:45:05 +0900

    a

--- rok-app/src/app/shared/service/aoo.service.ts
+++ rok-app/src/app/shared/service/aoo.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable, } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Aoo } from '../interface/aoo';
-import { error } from 'console';
 
 @Injectable({
   providedIn: 'root'
