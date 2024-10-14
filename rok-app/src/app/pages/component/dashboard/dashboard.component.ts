7a1ac4dfe0d92718bbf16a6e4e80d9fe67be01a0
betashort <wshota.betashort@gmail.com>
2024-10-14 19:52:50 +0900

    dashboard

--- rok-app/src/app/pages/component/dashboard/dashboard.component.ts
+++ rok-app/src/app/pages/component/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
